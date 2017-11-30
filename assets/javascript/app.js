b7784c53a6aea4a33d5bfaaa0d8ee19ce31ed2fa
Jessica Leigh <jleigh@pietech.com>
2017-11-29 20:02:56 -0500

    altered logic to make game more solid/unbreakable, added skippedGuesses

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,13 +3,20 @@ $( document ).ready(function(){
 
 	// setting up variables
 
-	var time = 6;
+	var time = 9;
 	var timer;
 	var correctGuesses = 0;
 	var incorrectGuesses = 0;
+	var q1rightGuess = 0; 
+	var q1wrongGuess = 0;
+	var q2rightGuess = 0;
+	var q2wrongGuess = 0;
+	var q3rightGuess = 0;
+	var q3wrongGuess = 0;
+	var skippedGuesses = 0;
 	//var guessNum = 0;
 	//var selectedAnswer;
-/*	var q1 = 
+	/*var q1 = 
 	{
 		question: "There are 50 states in the United States?",
 		options: ["True", "False"],
@@ -28,89 +35,102 @@ $( document ).ready(function(){
 		correctAnswer3: "True",
 	};
 
-
-
-
-	//setting up timer object
-	var Timer = 
-	{
-		count: 30;
-		reset: function() //how to reset timer
-		{
-			this.countdown = 30;
-			$('#game-timer).append('this.countdown + seconds remaining');
-		}
-
-	//defining how to start
-		start: function()
-		{
-			counter = setInterval(Timer.count, 1000);
-		}
 	}*/
+
 	$("#q1true").on("click", function()
 	{
-		correctGuesses++;
-		console.log(correctGuesses);
-		//guessNum++;
-
+		//correctGuesses++;
+		q1rightGuess = 1;
+		q1wrongGuess = 0;
+		/*if (correctGuesses >= 2) 
+		{
+			correctGuesses = 1;
+			alert("You already made your selection. Please continue to next question.");
+		}*/
 	})
 
+	/*$("#q1true, #q2false, #q3true").on("click", function()
+	{
+		correctGuesses++;
+		if(correctGuesses >= 2)
+		{
+			alert("You have already selected your answer.  Please continue onto the next question.");
+			correctGuesses
+		}
+
+	})*/
 
 	$("#q1false").on("click", function()
 	{
-		incorrectGuesses++;
-		console.log(incorrectGuesses);
-		//guessNum++;
+		q1rightGuess = 0;
+		q1wrongGuess = 1;
+		
 	})
+
+	/*$("#q1false, #q2true, #q3false").on("click", function()
+	{
+		incorrectGuesses++;
+	})*/
 
 
 	$("#q2true").on("click", function()
 	{
-		incorrectGuesses++;
-		console.log(incorrectGuesses);
-		//guessNum++;
+		q2rightGuess = 0;
+		q2wrongGuess = 1;
 	})
 
 
 	$("#q2false").on("click", function()
 	{
-		correctGuesses++;
-		console.log(correctGuesses);
-		//guessNum++;
+		q2rightGuess = 1;
+		q2wrongGuess = 0;
 	})
 
 
 	$("#q3true").on("click", function()
 	{
-		correctGuesses++;
-		console.log(correctGuesses);
-		//guessNum++;
-		done();
+		q3rightGuess = 1;
+		q3wrongGuess = 0;
+		/*done();
 		reset();
 		alert("Let's see how you did!");
-		//$('#rightGuesses').html('<p>' + 'Correct Guesses: ' + correctGuesses +'<p>');
-		//$('#wrongGuesses').html('<p>' + 'Incorrect Guesses: ' + incorrectGuesses + '<p>');
+		$('#rightGuesses').html('<p>' + 'Correct Guesses: ' + correctGuesses +'<p>');
+		$('#wrongGuesses').html('<p>' + 'Incorrect Guesses: ' + incorrectGuesses + '<p>');*/
 	})
 
 
 
 	$("#q3false").on("click", function()
 	{
-		incorrectGuesses++;
-		console.log(incorrectGuesses);
-		//guessNum++;
-		done();
+		q3rightGuess = 0;
+		q3wrongGuess = 1;
+		/*done();
 		reset();
 		alert("Let's see how you did!");
-		//$('#rightGuesses').html('<p>' + 'Correct Guesses: ' + correctGuesses +'<p>');
-		//$('#wrongGuesses').html('<p>' + 'Incorrect Guesses: ' + incorrectGuesses + '<p>');
+		$('#rightGuesses').html('<p>' + 'Correct Guesses: ' + correctGuesses +'<p>');
+		$('#wrongGuesses').html('<p>' + 'Incorrect Guesses: ' + incorrectGuesses + '<p>');*/
 	})
 
+   /*function guessesSkipped()
+    {
+    	if(correctGuesses > incorrectGuesses)
+      	{
+      		skippedGuesses = correctGuesses - incorrectGuesses;
+      	}
+      	else
+      	{
+      		skippedGuesses = incorrectGuesses - correctGuesses;
+     	}
+    }*/
 
 
 	function done() 
 	{
       clearInterval(timer);
+      correctGuesses = q1rightGuess + q2rightGuess + q3rightGuess;
+      incorrectGuesses = q1wrongGuess + q2wrongGuess + q3wrongGuess;
+      //guessesSkipped();
+      skippedGuesses = 3 - (correctGuesses + incorrectGuesses);
     }
 
 
@@ -118,7 +138,7 @@ $( document ).ready(function(){
     {
 
 		$('#game-timer').empty();
-    	time = 6;
+    	time = 9;
     }
 
 
@@ -151,9 +171,7 @@ $( document ).ready(function(){
 			reset();
 			$('#rightGuesses').html('<p>' + 'Correct Guesses: ' + correctGuesses +'<p>');
 			$('#wrongGuesses').html('<p>' + 'Incorrect Guesses: ' + incorrectGuesses + '<p>');
+			$('#skippedGuesses').html('<p>' + 'Missed Guesses: ' + skippedGuesses + '<p>');
 		});
-
-
-
 })
 
